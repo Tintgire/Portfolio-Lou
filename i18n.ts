e821f9446102a08c600c9bb237df599a333e4abc
Tintgire <boidinhugo14@gmail.com>
2026-05-11 16:24:49 +0200

    chore: remove unused imports (gsap, notFound) to clear ESLint warnings

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server';
-import { notFound } from 'next/navigation';
 
 export const locales = ['fr', 'en'] as const;
 export const defaultLocale = 'fr';
